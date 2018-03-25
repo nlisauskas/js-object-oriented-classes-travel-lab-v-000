a8a79196d8afe7458f35803d637243949d658240
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-25 21:07:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+class Driver {
+  constructor(name, day_joined) {
+    this.name = name;
+    this.day_joined = day_joined;
+  }
+
+  startDate() {
+    
+  }
+
+  yearsExperienceFromBeginningOf() {
+    
+  }
+}
 
