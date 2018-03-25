346d9fb7d69506c314e02b889bbf2aeb069bc865
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-25 21:21:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ class Route {
 
   let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue)
+  }
+
   blocksTravelled() {
     vertical = Math.abs(endingLocation.vertical - beginningLocation.vertical);
     horizontal = beginningLocation
