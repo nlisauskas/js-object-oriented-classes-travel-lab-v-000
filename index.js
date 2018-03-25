d0177f069c0c57df81aa63defc6d1284e915d0ab
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-25 21:33:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name;
+    this.date = date;
+    this.startDate = new Date(date);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return Math.abs(this.startDate.getFullYear() - year);
+  }
+}
+
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled() {
+    let verticalDistance = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    let horizontalDistance = Math.abs(this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal));
+    return verticalDistance + horizontalDistance;
+  }
+
+  estimatedTime(peak) {
+    if(peak) {
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
